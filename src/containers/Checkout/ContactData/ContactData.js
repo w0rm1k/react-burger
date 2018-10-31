7a65aa37df7c528151cfc0a6ae4f23d4ea8550d5
w0rm1k <viktor.sapsaj@gmail.com>
2018-10-31 23:26:23 +0500

    module 18 ready

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -135,7 +135,8 @@ class ContactData extends Component {
 		const order = {
 			ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
         this.props.onOrderBurger(order, this.props.token);
     }
@@ -203,7 +204,8 @@ const mapStateToProps = state => {
         ingredients: state.burgerBuilder.ingredients,
         totalPrice: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -15,5 +15,6 @@ export {
 export {
     auth,
     logout,
-    setAuthRedirectPath
+    setAuthRedirectPath,
+    authCheckState
 } from './auth';
